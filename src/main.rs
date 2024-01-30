1339b33ff087e8b32c49fd34635b4b5481488a35
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-01-29 20:51:06 -0800

    improvement: template rendered once, formatted clock to be nicer

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 // src/main.rs
 
 // dependencies
+use actix_files as fs;
 use actix_web::{http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
 use chrono::prelude::*;
+use std::sync::OnceLock;
 use tera::Tera;
+
+static COMPILED_TEMPLATE: OnceLock<Tera> = OnceLock::new();
+
+fn get_index_template() -> &'static Tera {
+    COMPILED_TEMPLATE.get_or_init(|| {
+        let mut index_template = Tera::new("templates/**/*").unwrap();
+        index_template
+            .add_template_file("templates/index.html", None)
+            .unwrap();
+        index_template
+    })
+}
 
 // function to get the current date and time
 fn get_today() -> String {
-    Local::now().to_string()
+    Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
+   
 }
 
 // function to set up the tera index template
 fn index_template() -> String {
-    let mut templates = Tera::new("templates/**/*").unwrap();
-    templates
-        .add_template_file("templates/index.html", None)
-        .unwrap();
     let mut context = tera::Context::new();
     context.insert("date", &get_today());
-    templates.render("index.html", &context).unwrap()
+    get_index_template().render("index.html", &context).unwrap()
 }
 
 // index route endpont handler; serves today's date and time as HTML
@@ -39,6 +50,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
+            .service(fs::Files::new("/", "screen.css").use_last_modified(true))
+            .service(fs::Files::new("/", "favicon.ico").use_last_modified(true))
             .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
