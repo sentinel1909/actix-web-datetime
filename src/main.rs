422bdbe4868b8aae71a0663b07446bfaf4e4a764
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-01-31 07:02:02 -0800

    improvement: add htmx

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,21 @@ fn get_index_template() -> Result<&'static Tera, Error> {
     })
 }
 
-// function to get the current date and time
+// function to get the current date
 fn get_today() -> String {
-    Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
+    Local::now().format("%Y-%m-%d").to_string()
+}
+
+// function to get the current time
+fn get_time() -> String {
+    Local::now().format("%H:%M:%S").to_string()
 }
 
 // function to set up the tera index template
 fn index_template() -> Result<String, Error> {
     let mut context = tera::Context::new();
     context.insert("date", &get_today());
+    context.insert("time", &get_time());
     get_index_template()?.render("index.html", &context)
 }
 
@@ -39,7 +45,7 @@ async fn index() -> impl Responder {
             .body(template),
         Err(e) => HttpResponse::InternalServerError()
             .content_type(ContentType::html())
-            .body(format!("{}", e)),
+            .body(format!("Error rendering template: {}", e)),
     }
 }
 
@@ -48,15 +54,25 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+// message endpoint handler; returns a simple greeting
+async fn message() -> impl Responder {
+    let msg = "Brought to you by htmx".to_string();
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(msg)
+}
+
 // main function
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
+            .route("/health_check", web::get().to(health_check))
+            .route("/message", web::get().to(message))
             .service(fs::Files::new("/", "screen.css").use_last_modified(true))
             .service(fs::Files::new("/", "favicon.ico").use_last_modified(true))
-            .route("/health_check", web::get().to(health_check))
+            .service(fs::Files::new("/", "htmx.min.js").use_last_modified(true))
     })
     .bind("127.0.0.1:8000")?
     .run()
